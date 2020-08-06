9ae23cd07e2e0d0d2b3a12b181df0eb80d78522e
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-06 22:47:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly[:railroads] = 4
-  monopoly[:railroads] = {}
-  monopoly[:railroads] = {}
+  monopoly[:rent_in_dollars] = {}
+  monopoly[:names] = {}
   
   
   monopoly

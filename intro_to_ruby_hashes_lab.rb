d185f6855d1a2ba500725d69f894d17a89367e5a
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-06 22:52:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
     railroads: {}
   }
   
-  monopoly[:railroads] = {pieces: 4}
+  monopoly[:railroads] = {:pieces => 4}
   monopoly[:railroads] = {:rent_in_dollars => {}}
   monopoly[:railroads] = {:names => {}}
   
